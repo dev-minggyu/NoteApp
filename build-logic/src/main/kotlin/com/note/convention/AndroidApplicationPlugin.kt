305f14a62ea2d5b896e25d4f6f2690e9c8a1053e
SMK <dev.minggyu@gmail.com>
2025-10-28 00:02:31 +0900

    build: cleanup application dependencies

--- build-logic/src/main/kotlin/com/note/convention/AndroidApplicationPlugin.kt
+++ build-logic/src/main/kotlin/com/note/convention/AndroidApplicationPlugin.kt
@@ -8,6 +8,8 @@ import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import com.note.Versions
+import gradle.kotlin.dsl.accessors._1b3a4bfcc6679b4a3e125bbd1caa7ba5.debugImplementation
+import gradle.kotlin.dsl.accessors._1b3a4bfcc6679b4a3e125bbd1caa7ba5.implementation
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.configureAndroidApplication() {
@@ -50,9 +52,30 @@ fun Project.configureAndroidApplication() {
             implementation(libs.findLibrary("kotlin.coroutines.android").get())
             implementation(libs.findLibrary("kotlin.serialization").get())
 
-            implementation(libs.findLibrary("compose.bom").get())
+            implementation(platform(libs.findLibrary("compose.bom").get()))
             implementation(libs.findLibrary("compose.foundation").get())
-            implementation(libs.findLibrary("compose.material").get())
+            implementation(libs.findLibrary("compose.material3").get())
+
+            implementation(libs.findLibrary("androidx.core").get())
+            implementation(libs.findLibrary("androidx.appcompat").get())
+            implementation(libs.findLibrary("androidx.constraintlayout").get())
+
+            implementation(libs.findLibrary("androidx.activity.ktx").get())
+            implementation(libs.findLibrary("androidx.activity.compose").get())
+            implementation(libs.findLibrary("androidx.fragment.ktx").get())
+
+            implementation(libs.findLibrary("androidx.lifecycle.viewmodel.ktx").get())
+            implementation(libs.findLibrary("androidx.lifecycle.extension").get())
+
+            implementation(libs.findLibrary("androidx.navigation.fragment.ktx").get())
+            implementation(libs.findLibrary("androidx.navigation.ui.ktx").get())
+
+            implementation(libs.findLibrary("androidx.splashscreen").get())
+            implementation(libs.findLibrary("androidx.preference").get())
+
+            implementation(libs.findLibrary("google.android.material").get())
+
+            debugImplementation(libs.findLibrary("leakcanary.android").get())
 
             implementation(libs.findLibrary("test.junit").get())
             implementation(libs.findLibrary("test.junit.extension").get())
