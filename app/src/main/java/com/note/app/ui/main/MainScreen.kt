2a6038d18161f52f8cf5fdbd59bb17e081e31a3f
SMK <smk940513@gmail.com>
2025-10-28 00:42:34 +0900

    feat: add extension for handling effect flow

--- app/src/main/java/com/note/app/ui/main/MainScreen.kt
+++ app/src/main/java/com/note/app/ui/main/MainScreen.kt
@@ -18,13 +18,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.note.app.utils.extension.HandleEffects
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -34,20 +34,18 @@ fun MainScreen(
     val state by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
 
-    LaunchedEffect(Unit) {
-        viewModel.uiEffect.collect { effect ->
-            when (effect) {
-                is MainContract.Event.ShowToast -> {
-                    Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
-                }
+    HandleEffects(viewModel.uiEffect) { effect ->
+        when (effect) {
+            is MainContract.Event.ShowToast -> {
+                Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
+            }
 
-                is MainContract.Event.NavigateToDetail -> {
-                    Toast.makeText(
-                        context,
-                        "상세 화면으로 이동: ${effect.id}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+            is MainContract.Event.NavigateToDetail -> {
+                Toast.makeText(
+                    context,
+                    "상세 화면으로 이동: ${effect.id}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
