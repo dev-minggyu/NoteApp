ebe65144d287470f67d00d6b760213d3270a2286
SMK <dev.minggyu@gmail.com>
2025-10-29 01:56:16 +0900

    feat: add note domain class

---
+++ domain/src/main/java/com/note/domain/repository/NoteRepository.kt
@@ -0,0 +1,13 @@
+package com.note.domain.repository
+
+import com.note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+    fun getAllNotes(): Flow<List<Note>>
+    suspend fun getNoteById(id: Long): Note?
+    suspend fun insertNote(note: Note): Long
+    suspend fun updateNote(note: Note)
+    suspend fun deleteNote(note: Note)
+    fun searchNotes(query: String): Flow<List<Note>>
+}
