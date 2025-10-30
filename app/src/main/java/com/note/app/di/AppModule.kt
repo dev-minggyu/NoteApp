4e946f13b8873563f6561c85098539e42c8ae5ca
SMK <dev.minggyu@gmail.com>
2025-10-31 01:56:22 +0900

    feat: implement note detail screen

--- app/src/main/java/com/note/app/di/AppModule.kt
+++ app/src/main/java/com/note/app/di/AppModule.kt
@@ -3,6 +3,9 @@ package com.note.app.di
 import com.note.app.ui.main.MainProcessor
 import com.note.app.ui.main.MainReducer
 import com.note.app.ui.main.MainViewModel
+import com.note.app.ui.notedetail.NoteDetailProcessor
+import com.note.app.ui.notedetail.NoteDetailReducer
+import com.note.app.ui.notedetail.NoteDetailViewModel
 import com.note.app.ui.splash.InitProcessor
 import com.note.app.ui.splash.InitReducer
 import com.note.app.ui.splash.InitViewModel
@@ -10,15 +13,15 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    // Processors
-    factory { InitProcessor() }
-    factory { MainProcessor(get()) }
-    
-    // Reducers
-    factory { InitReducer() }
-    factory { MainReducer() }
-    
-    // ViewModels
     viewModel { InitViewModel(get(), get()) }
+    factory { InitReducer() }
+    factory { InitProcessor() }
+
     viewModel { MainViewModel(get(), get()) }
+    factory { MainReducer() }
+    factory { MainProcessor(get()) }
+
+    viewModel { NoteDetailViewModel(get(), get()) }
+    factory { NoteDetailReducer() }
+    factory { NoteDetailProcessor(get()) }
 }
