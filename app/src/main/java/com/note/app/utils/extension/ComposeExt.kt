21e2cb19fa344b494ad5e308499506088d5f8ac6
SMK <dev.minggyu@gmail.com>
2025-10-31 01:52:29 +0900

    feat: change flow collect method in HandleEffects

--- app/src/main/java/com/note/app/utils/extension/ComposeExt.kt
+++ app/src/main/java/com/note/app/utils/extension/ComposeExt.kt
@@ -2,6 +2,9 @@ package com.note.app.utils.extension
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
 import com.note.app.base.contract.UiEvent
 import kotlinx.coroutines.flow.Flow
 
@@ -10,7 +13,10 @@ fun <T : UiEvent> HandleEffects(
     effectFlow: Flow<T>,
     onEffect: (T) -> Unit
 ) {
-    LaunchedEffect(Unit) {
-        effectFlow.collect(onEffect)
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(effectFlow, lifecycleOwner) {
+        effectFlow
+            .flowWithLifecycle(lifecycleOwner.lifecycle, Lifecycle.State.STARTED)
+            .collect(onEffect)
     }
 }
