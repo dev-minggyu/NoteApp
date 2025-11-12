f361c12d22d8358747c3e211df279fae09134f99
SMK <smk940513@gmail.com>
2025-11-13 01:42:16 +0900

    feat: add navigation transition to DetailScreen

--- app/src/main/java/com/note/app/ui/notedetail/NoteDetailNavGraph.kt
+++ app/src/main/java/com/note/app/ui/notedetail/NoteDetailNavGraph.kt
@@ -4,11 +4,16 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.note.app.ui.navigation.Screen
+import com.note.app.utils.extension.slideInLeft
+import com.note.app.utils.extension.slideOutRight
 
 fun NavGraphBuilder.detailNavGraph(
     popBackStack: () -> Unit,
 ) {
-    composable<Screen.NoteDetail> { backStackEntry ->
+    composable<Screen.NoteDetail>(
+        enterTransition = { slideInLeft(durationMillis = 1000) },
+        popExitTransition = { slideOutRight(durationMillis = 1000) }
+    ) { backStackEntry ->
         val args = backStackEntry.toRoute<Screen.NoteDetail>()
         NoteDetailScreen(
             noteId = args.noteId,
