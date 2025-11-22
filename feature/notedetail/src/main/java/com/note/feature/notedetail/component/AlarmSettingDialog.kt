41c16984b743a9b5737f765ca915dcd0fc1e39ce
SMK <dev.minggyu@gmail.com>
2025-11-23 01:25:39 +0900

    feat: update some color values

--- feature/notedetail/src/main/java/com/note/feature/notedetail/component/AlarmSettingDialog.kt
+++ feature/notedetail/src/main/java/com/note/feature/notedetail/component/AlarmSettingDialog.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -241,6 +242,9 @@ fun AlarmSettingDialog(
 
     if (showDatePicker) {
         DatePickerDialog(
+            colors = DatePickerDefaults.colors(
+                containerColor = AppTheme.colors.contentBackground,
+            ),
             onDismissRequest = { showDatePicker = false },
             confirmButton = {
                 TextButton(
@@ -272,6 +276,7 @@ fun AlarmSettingDialog(
 
     if (showTimePicker) {
         AlertDialog(
+            containerColor = AppTheme.colors.contentBackground,
             onDismissRequest = { showTimePicker = false },
             confirmButton = {
                 TextButton(
